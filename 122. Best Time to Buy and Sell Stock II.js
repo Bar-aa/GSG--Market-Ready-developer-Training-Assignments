4a7e282aed33e3ff7d64b143d69d2c03a827eed2
Bara'a Shelbaya <133972448+Bar-aa@users.noreply.github.com>
2024-09-10 00:01:46 +0300

    Create 122. Best Time to Buy and Sell Stock II.js

---
+++ 122. Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+function maxProfit(prices) {
+    let totalProfit = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            totalProfit += prices[i] - prices[i - 1];
+        }
+    }
+    
+    return totalProfit;
+}
